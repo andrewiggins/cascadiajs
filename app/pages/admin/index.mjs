7587d887fc3942d4865e17ce36537d07c342d3a1
Andre Wiggins <459878+andrewiggins@users.noreply.github.com>
2026-06-02 14:35:32 -0700

    Move QR code generator into admin

--- app/pages/admin/index.mjs
+++ app/pages/admin/index.mjs
@@ -5,6 +5,8 @@ export default function ({ html /*state*/ }) {
                 <li><a href="/admin/events">List Events</a></li>
                 <li><a href="/admin/speakers">List Speakers</a></li>
                 <li><a href="/admin/talks">List Talks</a></li>
+                <li><a href="/admin/qr-code">QR Code Generator</a></li>
+            </ul>
         </admin-layout>
     `
 }
